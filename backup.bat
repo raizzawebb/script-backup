8d8e2494fddbcf6a49da88efdd0e924188d92ff3
RAIZA SOARES PIRES <raiza.spires@senacsp.edu.br>
2024-10-21 16:21:59 -0300

    v. 1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir BACKUP
+copy \windows\*.txt\users\raiza.spires\documents\script-backup\BACKUP
+copy \windows\system32\*.log \users\raiza.spires\documents\script-backup\BACKUP
+copy \windows\system32\*.txt \users\raiza,spires\documents\script-backup\BACKUP
